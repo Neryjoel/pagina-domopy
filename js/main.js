e47ff49b627152307ea46689e3fee117b5bb3d12
Danesa Deposito <deposito.danesa@casanissei.loc>
2025-05-05 10:40:58 -0300

    Primer commit local

---
+++ js/main.js
@@ -0,0 +1,123 @@
+(function ($) {
+    "use strict";
+/*
+    // Navbar on scrolling
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.navbar').fadeIn('slow').css('display', 'flex');
+        } else {
+            $('.navbar').fadeOut('slow').css('display', 'none');
+        }
+    });
+
+*/
+    // Smooth scrolling on the navbar links
+$(".navbar-nav a, .btn-scroll").on('click', function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        const target = this.hash;
+        const offset = $(target).offset().top - $('.navbar').outerHeight();
+
+        const setActiveLink = () => {
+            $('.navbar-nav .active').removeClass('active');
+            $(`.navbar-nav a[href="${target}"]`).addClass('active');
+        };
+
+        if ($('.navbar-collapse').hasClass('show')) {
+            $('.navbar-collapse').collapse('hide');
+            setTimeout(() => {
+                $('html, body').animate({ scrollTop: offset }, 'swing', setActiveLink);
+            }, 400); // espera a que termine el colapso
+        } else {
+            $('html, body').animate({ scrollTop: offset }, 'swing', setActiveLink);
+        }
+    }
+});
+
+
+    // Scroll to Bottom
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scroll-to-bottom').fadeOut('slow');
+        } else {
+            $('.scroll-to-bottom').fadeIn('slow');
+        }
+    });
+
+
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Gallery carousel
+    $(".gallery-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1500,
+        dots: false,
+        loop: true,
+        nav : true,
+        touchDrag: false,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            992:{
+                items:4
+            },
+            1200:{
+                items:5
+            }
+        }
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+    });
+    
+})(jQuery);
+
